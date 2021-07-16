8271f9d5b5134f919fa45b3ab2c3f9bf30a83e23
hellodebs <debora.behrens@outlook.com>
2021-07-16 11:30:28 +0200

    Create static object array of todo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import Input from "./components/Input.js";
 import Button from "./components/Button.js";
 
 function App() {
+  const todoItem = [
+    {
+      id: 1,
+      todo: "clean",
+      isDone: false,
+    },
+    {
+      id: 2,
+      todo: "homework",
+      isDone: false,
+    },
+    {
+      id: 3,
+      todo: "read",
+      isDone: false,
+    },
+  ];
+  console.log(todoItem);
+
   return (
     <div className="App">
       <h1>Todo App</h1>
