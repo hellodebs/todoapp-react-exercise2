00f22d432876978bb196bfbb8f30db71e1bf29de
hellodebs <debora.behrens@outlook.com>
2021-07-16 11:22:16 +0200

    Add component folder, create iInput and Button component functions, import and export creates .js files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import Input from "./components/Input.js";
+import Button from "./components/Button.js";
 
 function App() {
   return (
     <div className="App">
-      <input type="text" />
-      <button>Add</button>
+      <h1>Todo App</h1>
+      <Input />
+      <Button />
     </div>
   );
 }
 
 export default App;
+
+/* I have Components: Input and button */
